b1118e8117c84b62a4fd143061b6df3c86ea16ef
Sebastien Schaeffler <bidisoft@gmail.com>
2017-02-10 07:00:29 +0100

    Apps endpoints

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var bodyParser = require('body-parser');
 
-const fileName = './apis.json';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.use(express.bodyParser());
+const apis = JSON.parse(fs.readFileSync('./apis.json', 'utf8'));
+const apps =  JSON.parse(fs.readFileSync('./apps.json', 'utf8'));
 
 // Accept CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
   next();
 });
 
+// api
+
 app.get('/api', function(req, res) {
-  var data =  fs.readFileSync(fileName, 'utf8');
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(data);
-  res.end();
+  res.json(apis);
 });
 
 app.get('/api/random', function(req, res) {
@@ -37,13 +42,23 @@ app.get('/api/:id', function(req, res) {
 });
 
 app.post('/api', function(req, res) {
-  if(!req.body.hasOwnProperty('name')) {
+  console.log("POST: ", req.body);
+  if(!req.body.params.hasOwnProperty('name')) {
     res.statusCode = 400;
     return res.send('Error 400: Post syntax incorrect.');
   }
 
   var newApi = {
-    name : req.body.name
+    id : apis.length,
+    name: req.body.params.name,
+    technical_name: req.body.params.technical_name,
+    context: req.body.params.context,
+    version: req.body.params.version,
+    visibility: req.body.params.visibility,
+    description:req.body.params.description,
+    tags:req.body.params.tags,
+    api_endpoint:req.body.params.api_endpoint,
+    doc_endpoint:req.body.params.doc_endpoint
   };
 
   apis.push(newApi);
@@ -59,5 +74,91 @@ app.delete('/api/:id', function(req, res) {
   apis.splice(req.params.id, 1);
   res.json(true);
 });
+
+// app
+
+app.get('/app', function(req, res) {
+  res.json(apps);
+});
+
+app.get('/app/random', function(req, res) {
+  var id = Math.floor(Math.random() * apps.length);
+  var q = apps[id];
+  res.json(q);
+});
+
+app.get('/app/:id', function(req, res) {
+  if(apps.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No app found');
+  }
+
+  var q = apps[req.params.id];
+  res.json(q);
+});
+
+app.post('/app', function(req, res) {
+  console.log("POST: ", req.body);
+  if(!req.body.params.hasOwnProperty('name')) {
+    res.statusCode = 400;
+    return res.send('Error 400: Post syntax incorrect.');
+  }
+
+  var newApp = {
+    id : apps.length,
+    name : req.body.params.name,
+    description : req.body.params.description,
+    callback_url: req.body.params.callback_url
+  };
+
+  apps.push(newApp);
+  res.json(true);
+});
+
+app.put('/app/:id', function(req, res) {
+  console.log("UPDATE: ", req.body);
+  if(!req.body.params.hasOwnProperty('name')) {
+    res.statusCode = 400;
+    return res.send('Error 400: Post syntax incorrect.');
+  }
+
+  var newApp = {
+    id : apps.length,
+    name : req.body.params.name,
+    description : req.body.params.description,
+    callback_url: req.body.params.callback_url
+  };
+
+  apps[eq.params.id] = newApp;
+
+  res.json(true);
+});
+
+app.patch('/app/:id', function(req, res) {
+  console.log("PATCH: ", req.body);
+  if(!req.body.params.hasOwnProperty('id') || req.body.params.hasOwnProperty('id') === '') {
+    res.statusCode = 400;
+    return res.send('Error 400: Post syntax incorrect: id is undefined or empty');
+  }
+
+  apps[req.params.id].name=req.body.params.name;
+  apps[req.params.id].description=req.body.params.description;
+  apps[req.params.id].callback_url=req.body.params.callback_url;
+
+  res.json(true);
+});
+
+app.delete('/app/:id', function(req, res) {
+  console.log("DELETE: ", req.body);
+  if(apps.length <= req.params.id) {
+    res.statusCode = 404;
+    return res.send('Error 404: No app found');
+  }
+
+  apps.splice(req.params.id, 1);
+  res.json(true);
+});
+
+// Subscription
 
 app.listen(process.env.PORT || 3100);
